f58ea4bf6b8b6d7bc48682310a1226b9c66bb7e8
stvnlee890 <stvnlee890@gmail.com>
2023-01-29 14:54:05 -0800

    test

--- 02.landing-page/app.js
+++ 02.landing-page/app.js
@@ -1,10 +1,10 @@
-let tl = gsap.timeline({defaults: {ease: "power4.inOut", duration: 2}})
-let flagPoles = CSSRulePlugin.getRule(".card:before"); 
+let tl = gsap.timeline({ defaults: { ease: "power4.inOut", duration: 2 } });
+let flagPoles = CSSRulePlugin.getRule(".cards::before")
+let transitions = CSSRulePlugin.getRule(".cards:hover::before")
 
 tl.to('h1', { 'clip-path': 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)', opacity: 1, y: 0, duration: 2.2})
-tl.to('.form', { 'clip-path': 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)', opacity: 1, y: 0}, "-=2")
-tl.from('.card', { scaleY: 0, stagger: .2}, "-=2")
-tl.from(flagPoles, {  stagger: 1, opacity: 0, transform: 'translateY(100px)' }, "-=2")
-tl.to('.title, .desc', { stagger: .1, duration: 1.2, opacity: 1, y: 0}, "-=2")
-tl.to('footer', { opacity: 1}, "-=2")
-
+.to('.box', { 'clip-path': 'polygon(0% 110%, 110% 110%, 110% 0%, 0% 0%)', opacity: 1, y: 0, duration: 2.5}, "-=2")
+.to('.inner-box', { opacity: .5, y: 0, duration: .2},  "-=1")
+.from('.cards', { opacity: 0, scaleY: 0, stagger: .3 }, "-=2")
+.from(flagPoles, { duration: 3, opacity: 0, transform: "translateY(250px)" }, "-=3")
+.from('.title-desc, .desc', { stagger: .1, duration: 1, opacity: 0, y: "100px" }, "-=3")
