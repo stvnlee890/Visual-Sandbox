13e38365a6fd7b8a28c9af229b41e757f41a7e99
stvnlee890 <stvnlee890@gmail.com>
2023-02-22 19:35:02 -0800

    add comment

--- 03.react-gsap/src/components/works/Works.js
+++ 03.react-gsap/src/components/works/Works.js
@@ -5,13 +5,14 @@ import { animateWorks } from "./works-animation";
 const Works = () => {
   const borders = useRef(null);
 
+  // can target children nodes to iterative over each node and apply animation
   useLayoutEffect(() => {
     const refArr = [...borders.current.children];
     refArr.forEach((ele) => {
-        if (ele.className.includes("works-border")) animateWorks(ele);
-      //   console.log(ele.className.includes("works-border"));
+      if (ele.className.includes("works-border")) animateWorks(ele);
     });
   }, []);
+
   return (
     <div ref={borders} className="works-container">
       <div className="works-1">WORKS 1</div>
